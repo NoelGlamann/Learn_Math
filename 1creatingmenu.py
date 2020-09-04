d127053ef584a87741b0c87925faf9fd996f5e08
Noel Glamann <nglamann@debian08.CIS.COM>
2020-09-04 08:14:39 -0500

    new file:   1creatingmenu.py

---
+++ 1creatingmenu.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+#Noel Glamann
+#Last edit made to file: September 4, 2020
+
+''' This is the first file created to start my project Learn Math.
+I will be developing a program with a variety of math files and 
+lessons to help users with their most exciting work. '''
+
+#----IMPORTS-----------------------------------------------------
+
+
+
+#----FUNCTIONS---------------------------------------------------
+
+def menu():
+    print()
+    print('''        WELCOME TO 
+        LEARN MATH
+    ------------------''')
+    print()
+    print("Let's get studying!")
+    print('''
+    1. option1
+    2. option2
+    3. option3
+    4. option4
+    ''')
+    choice = input("What are we studying today (1, 2, 3...) ? ")
+    print(choice)
+
+#----PROGRAM-----------------------------------------------------
+
+menu()
